54e858c43bf6eb71879c8b86321abbcf88522487
Alex <alexduval71@gmail.com>
2016-08-09 17:02:15 +0200

    add Markdown as helper

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,22 +1,42 @@
 class ArticleController < ApplicationController
   def index
+    @article = Article.all
   end
 
   def create
+    @article = Aritcle.new(article_params)
+    @article.save
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article.id)
+
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_index_path
   end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,2 +1,7 @@
 module ArticleHelper
+
+  def to_markdown(str)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown.render(str).html_safe
+  end
 end
